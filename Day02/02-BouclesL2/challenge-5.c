dc3cc487f28075c5e44e9760653fc64b657f9420
oussama <outaroussama88@gmail.com>
2024-09-04 15:59:32 +0100

    ajout challenge 5 boucles

--- Day02/02-BouclesL2/challenge-5.c
+++ Day02/02-BouclesL2/challenge-5.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main () {
-    
+    int N, S;
+    printf("Donner un nombre : ");
+    scanf("%d", &N);
+    for(int i=1; i<=N; i++) S += i;
+    printf("la somme = %d", S);
     return 0;
 }
