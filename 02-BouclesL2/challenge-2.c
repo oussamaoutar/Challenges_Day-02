0c61891e22aaa3a412cb9b685e7f18570c81e5d4
Oussama Outar <89809444+oussamaoutar@users.noreply.github.com>
2024-09-06 14:32:41 +0100

    Update Boucle

--- 02-BouclesL2/challenge-2.c
+++ 02-BouclesL2/challenge-2.c
@@ -6,7 +6,7 @@ int main () {
     scanf("%d",&L);
     for(int i=0; i<L; i++) {
         for(int j=i+1; j<L ; j++) printf(" ");
-        for(int j=0; j<(i+1)*2-1; j++) printf("*");
+        for(int j=0; j<2*i+1; j++) printf("*");
         printf("\n");
     }
     return 0;
